8554d3ddf97c983f99f858b8d1021aba969b9415
Tom <tdhw22@gmail.com>
2022-10-04 19:23:14 +0300

    Refactored errorhandler.js with http-status-codes and created a defaultError object for easier error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,19 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: err });
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Somthing went wrong try again later",
+  };
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join("");
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+  // res.status(defaultError.statusCode).json({ msg: err });
 };
 
 export default errorHandlerMiddleware;
