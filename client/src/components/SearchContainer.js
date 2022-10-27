1523a773a6863c2ac504e2074a2bc9ce19104dc7
Tom <tdhw22@gmail.com>
2022-10-27 14:24:37 +0300

    Completed the SearchContainer.js with the functionality in both front and back ends

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,7 +1,83 @@
 import React from "react";
+import { FormRow, FormRowSelect } from ".";
+import Wrapper from "../assets/wrappers/SearchContainer";
+import { useAppContext } from "../context/AppContext";
 
 const SearchContainer = () => {
-  return <h1>SearchContainer</h1>;
+  const {
+    isLoading,
+    search,
+    searchType,
+    searchStatus,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
+
+  const handleSearch = (e) => {
+    if (isLoading) return;
+    handleChange({ name: e.target.name, value: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>Search form</h4>
+        {/* search position */}
+        <div className="form-container">
+          <FormRow
+            labelText="position"
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+
+          {/* search by status */}
+          <FormRowSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={["all", ...statusOptions]}
+          />
+
+          {/* search by type */}
+          <FormRowSelect
+            labelText="type"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={["all", ...jobTypeOptions]}
+          />
+
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            Clear Filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
