ab04ee73513263967a24298804db20d207e486a2
Tom <tdhw22@gmail.com>
2023-03-05 13:58:21 +0200

    Added a debounce function in SearchContainer and initiated new git repository beacuse last one got deleted

---
+++ client/src/pages/dashboard/AllJobs.js
@@ -0,0 +1,13 @@
+import { JobsContainer, SearchContainer } from "../../components";
+import React from "react";
+
+const AllJobs = () => {
+  return (
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+  );
+};
+
+export default AllJobs;
