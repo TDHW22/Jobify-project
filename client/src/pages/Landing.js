5c270dffba7ff7ca986e0400000572892bf47f1c
Tom <tdhw22@gmail.com>
2023-03-05 17:36:47 +0200

    Changed from LocalStorage to browser cookies and added a cookie parser dependancy

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,33 +1,38 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import React from "react";
+import { useAppContext } from "../context/appContext";
 
 const Landing = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className="container page">
-        {/* info */}
-        <div className="info">
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>This site is a site for tracking job applications.</p>
-          <p>
-            You can choose the demo user but take in mind that you cant edit any
-            information for that i recommend you register.
-          </p>
-          <Link to="/register" className="btn btn-hero">
-            Login/Register
-          </Link>
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>This site is a site for tracking job applications.</p>
+            <p>
+              You can choose the demo user but take in mind that you cant edit
+              any information for that i recommend you register.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </React.Fragment>
   );
 };
 
--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -1,9 +1,12 @@
 import { useAppContext } from "../context/appContext";
 import { Navigate } from "react-router-dom";
 import React from "react";
+import Loading from "../components/Loading.js";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAppContext();
+  const { user, userLoading } = useAppContext();
+
+  if (userLoading) return <Loading />;
   if (!user) {
     return <Navigate to="/landing" />;
   }
